1c5c0aeadaa7be58087c29567291c0a4934918f4
jondagamkd <johntaylorwoodland@gmail.com>
2022-01-08 01:11:51 -0800

    deployment

---
+++ client/build/precache-manifest.1b8a30d6223303ed8879c5cf8deab865.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80d18d9724e4f46798417a39777cfd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04622e748552513391de",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "183ecf38862b767fe164",
+    "url": "/static/js/2.28f58591.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.28f58591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04622e748552513391de",
+    "url": "/static/js/main.ed1c1795.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
